5d1628d1004eea1c93e5f43e85afe1b234c4f2f7
DavSan <david.sanchez.martinez@outlook.com>
2024-08-12 14:57:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Retrieve the value entered in the input field as a string and convert this string to a floating-point number
 const numberOne = parseFloat(document.getElementById('number1').value); 
 const numberTwo = parseFloat(document.getElementById('number2').value);
+
+function calculate(numberOne, numberTwo) {
+    if (!isNaN(numberOne) && !isNaN(numberTwo)) {
+        return alert("Please, introduce a valid number")
+    }
+}
