9ed434cc1ccdd5a57aaa91efb3c83885212ee24d
DavSan <david.sanchez.martinez@outlook.com>
2024-08-12 14:47:02 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// Retrieve the value entered in the input field as a string and convert this string to a floating-point number
+const numberOne = parseFloat(document.getElementById('number1').value); 
+const numberTwo = parseFloat(document.getElementById('number2').value);
